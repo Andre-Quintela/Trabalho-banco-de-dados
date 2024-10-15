954b4d05f6947573258d58b670d9916d6d443b47
André <andre.lsquintela@gmail.com>
2024-10-15 18:51:36 -0300

    Removendo framework, trabalho será realizado puramente com python

--- pacientes/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PacientesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pacientes'
--- pacientes/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def pacientes(request):
-    return render(request, '../templates/_layout.html')
